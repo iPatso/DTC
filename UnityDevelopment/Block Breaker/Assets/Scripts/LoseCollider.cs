a8de6d33f5c386e97548965aea456529a5ff30ac
Patrick Simpelo <Patrick.simpelo@gmail.com>
2016-02-10 16:30:49 -0800

    Progress on Block Breaker game. Ball craeted.

---
+++ UnityDevelopment/Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+	
+	public LevelManager levelManager; //Placed so can see in GUI
+
+	//Should not be called because we are not using triggers
+	void OnTriggerEnter2D (Collider2D trigger) {
+		print ("Trigger");
+		levelManager.LoadLevel("Win Screen");
+	}
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		print ("Collision");
+	}
+}
