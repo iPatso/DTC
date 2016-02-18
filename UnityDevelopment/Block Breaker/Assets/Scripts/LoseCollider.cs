40529bf95d48633c273b97370c7896254990f076
Patrick Simpelo <Patrick.simpelo@gmail.com>
2016-02-18 15:15:18 -0800

    Finished the base of the Block Breaker Game

--- UnityDevelopment/Block Breaker/Assets/Scripts/LoseCollider.cs
+++ UnityDevelopment/Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -3,12 +3,12 @@ using System.Collections;
 
 public class LoseCollider : MonoBehaviour {
 	
-	public LevelManager levelManager; //Placed so can see in GUI
+	private LevelManager levelManager;
 
 	//Should not be called because we are not using triggers
 	void OnTriggerEnter2D (Collider2D trigger) {
-		print ("Trigger");
-		levelManager.LoadLevel("Win Screen");
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+		levelManager.LoadLevel("Lose Screen");
 	}
 
 	void OnCollisionEnter2D (Collision2D collision) {
